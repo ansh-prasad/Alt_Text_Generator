ea4ece7f68f41180feb3f9f7ba37e197f44dcd36
ansh-prasad <anshprasad01@gmail.com>
2025-02-10 17:54:38 +0530

    Remove unnecessary Django app files and code

--- doc_to_alt_text/extractor/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-class UploadDocxForm(forms.Form):
-    doc_file = forms.FileField()
--- doc_to_alt_text/extractor/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.upload_docx, name='upload_docx'),
-    path('results/', views.view_results, name='view_results'),
-]
